35919d1967ab6cb70b125d618869b0fc9e7d055a
skys215 <skys215@gmai.com>
2014-01-25 22:52:04 +0800

    Updated detail dailog box

--- field_application/field_application/static/scripts/popupBox.js
+++ field_application/field_application/static/scripts/popupBox.js
@@ -51,6 +51,10 @@ function showAppList( applAry ){
 		 height: 300,
 
 		   lock: true,
+		 button:[{
+		 	    name: '关闭',
+		 	callback: function(){ this.close(); },
+		 }]
 	})
 
 }
@@ -66,20 +70,20 @@ function genAppInfoTable( detail ){
 		{ index: "date"                   , 'text': "使用日期"          },	
 		{ index: "time"                   , 'text': "使用时间"          },
 		{ index: "activity_summary"       , 'text': "活动简介"          },
-		{ index: "plan_file"              , 'text': "活动策划文件下载"  },
-		{ index: "remarks"                , 'text': "备注"              },
+		{ index: "plan_file"              , 'text': "策划文件"          },
+	  //{ index: "remarks"                , 'text': "备注"              },
 
-		{ index: "sponsor"                , 'text': "赞助商"            },
-		{ index: "sponsorship"            , 'text': "赞助金额及物品"    },
-		{ index: "sponsorship_usage"      , 'text': "赞助财物用途"      },
+	  //{ index: "sponsor"                , 'text': "赞助商"            },
+	  //{ index: "sponsorship"            , 'text': "赞助金额及物品"    },
+	  //{ index: "sponsorship_usage"      , 'text': "赞助财物用途"      },
 
-		{ index: "approved"               , 'text': "审批情况"          },
-		{ index: "application_time"       , 'text': "申请时间"          },
+	  //{ index: "approved"               , 'text': "审批情况"          },
+	  //{ index: "application_time"       , 'text': "申请时间"          },
 	]
 	detail = eval('('+detail+')');
 
 	var appFormTable = document.createElement( 'table' );
-	appFormTable.className = 'app_table';
+	appFormTable.className = 'app_table app_info_table';
 
 	var i;
 	for( i = 0; i < indexToText.length; i++ ){
@@ -88,13 +92,19 @@ function genAppInfoTable( detail ){
 		appFormTableTR = appFormTable.insertRow();
 
 			appFormTableTD = appFormTableTR.insertCell();
+			appFormTableTD.className = 'app_info_title';
 		    appFormTableTD.appendChild( document.createTextNode( indexToText[i]['text'] + "：" ) );
 
 		    appFormTableTD = appFormTableTR.insertCell();
+			appFormTableTD.className = 'app_info_cont';
+
 		    var insertText = '';
 		    switch( indexToText[i]['index'] ){
 		    	case 'plan_file':
-		    		insertText = '<a href="' + detail[indexToText[i]['index']] + '">文件下载</a>';
+		    		if( detail[indexToText[i]['index']] !='' )
+		    			insertText = '<a href="' + detail[indexToText[i]['index']] + '">下载</a>';
+		    		else
+		    			insertText = '无文件可供下载';	
 		    		break;
 		    	case 'approved':
 		    		console.log( typeof detail['approved']);
@@ -130,20 +140,27 @@ function showAppForm( appId ){
 	xmlhttp.onreadystatechange=function(){
 		if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ){
 
-			content = genAppInfoTable( xmlhttp.responseText );
+			content = genAppInfoTable( xmlhttp.responseText  );
 
 			var artdialog = art.dialog({
+				     id: "app_info",
 				  title: "",
 				content: content,
 
 				  width: 500,
-				 height: 300,
+				 height: 300,	
 
 				   lock: true,
+				 button: [
+				 	{
+				 		    name: "返回",
+				 		callback: function(){ this.close() },
+				 	}
+				 ],
 			})
 		}
 	}
 
-	xmlhttp.open( "GET", "../message/?id=" + appId , false );
+	xmlhttp.open( "GET", "../get_detail/?id=" + appId , false );
 	xmlhttp.send();
-}
+};
