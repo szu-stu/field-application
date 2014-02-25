ee9aa55effa30ab04e915ce68d2fa1be05fdc985
doyoubi <doyoubihgx@gmail.com>
2014-02-25 15:56:56 +0800

    add table download

--- field_application/field_application/urls.py
+++ field_application/field_application/urls.py
@@ -1,6 +1,10 @@
+import os
+
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
+
+from field_application import settings
 
 
 admin.autodiscover()
@@ -8,6 +12,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'),
                name='home'),
+    url(r'^table_download$',
+        TemplateView.as_view(template_name='table_download.html'),
+               name='table_download'),
     url(r'^deny/$', TemplateView.as_view(template_name='deny.html'),
                name='deny'),
     url(r'^south_stadium/', include('field_application.south_stadium.urls',
@@ -29,3 +36,10 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# serve media file when using developing server
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^uploaded_file/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT,}),
+        )
