74277e9f129f3b3c8701b4c26b5a55ba7989ed5b
bingo <icebin000@gmail.com>
2014-04-24 01:07:38 +0800

    finish the document app

--- field_application/field_application/document/urls.py
+++ field_application/field_application/document/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, patterns
 from django.conf import settings
 from django.conf.urls.static import static
-from views import Upload_File,del_doc
+from views import Upload_File,del_doc,index
 
 urlpatterns = patterns('',
-    url(r'^$',  Upload_File, name='doc_download'),
+    url(r'^$',  index, name='doc_index'),
     url(r'^delete/$', del_doc , name='del_doc'),
+    url(r'^modify/$', Upload_File , name='doc_upload'),
 
 )+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- field_application/field_application/document/views.py
+++ field_application/field_application/document/views.py
@@ -1,9 +1,18 @@
+#-*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
 
 from models import Document
 from forms import DocumentForm
 
+from field_application.account.permission import check_perms
+
+def index(request):
+    form=DocumentForm()
+    doc_list=Document.objects.all()
+    return render(request,'document/index.html',{'form':form,'doc_list':doc_list})
+
+@check_perms('account.manager', u'无管理权限')
 def Upload_File(request):
     form=DocumentForm()
     doc_list=Document.objects.all()
@@ -13,13 +22,14 @@ def Upload_File(request):
         if not form.is_valid():
             return render(request,'document/document.html',{'form':form, 'doc_list':doc_list})
         form.save()
-        return HttpResponseRedirect('/document')
+        return HttpResponseRedirect('/document/modify')
 
     else:
-        return render(request,'document/document.html',{'doc_list':doc_list})
+        return render(request,'document/document.html',{'form':form,'doc_list':doc_list})
 
+@check_perms('account.manager', u'无管理权限')
 def del_doc(request):
     doc_id=request.GET.get('id')
     doc=get_object_or_404(Document, id=doc_id)
     doc.delete()
-    return HttpResponseRedirect('/document')
+    return HttpResponseRedirect('/document/modify')
